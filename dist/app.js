4c47271d287df1e10458b7490357c4c2b8209350
Semos <semos@semosedu.local>
2026-02-17 17:31:26 +0100

    post html file amd header scss file

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+(()=>{"use strict";var r={"./src/styles/app.scss"(r,e,o){o.r(e)}},e={};function o(t){var s=e[t];if(void 0!==s)return s.exports;if(void 0===r[t]){var n=new Error("Cannot find module '"+t+"'");throw n.code="MODULE_NOT_FOUND",n}var a=e[t]={exports:{}};return r[t](a,a.exports,o),a.exports}o.r=r=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(r,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(r,"__esModule",{value:!0})};var t={};(()=>{
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+o.r(t);o(/*! ./styles/app.scss */"./src/styles/app.scss")})()})();
+//# sourceMappingURL=app.js.map
---
+++ dist/editor.js
@@ -0,0 +1,6 @@
+(()=>{"use strict";var r={"./src/styles/editor.scss"(r,e,o){o.r(e)}},e={};function o(t){var s=e[t];if(void 0!==s)return s.exports;if(void 0===r[t]){var i=new Error("Cannot find module '"+t+"'");throw i.code="MODULE_NOT_FOUND",i}var n=e[t]={exports:{}};return r[t](n,n.exports,o),n.exports}o.r=r=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(r,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(r,"__esModule",{value:!0})};var t={};(()=>{
+/*!***********************!*\
+  !*** ./src/editor.js ***!
+  \***********************/
+o.r(t);o(/*! ./styles/editor.scss */"./src/styles/editor.scss")})()})();
+//# sourceMappingURL=editor.js.map
